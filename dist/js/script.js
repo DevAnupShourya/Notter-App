8db3c924ab849dd9d362bae26b1534d8789f4143
Anup Shourya <contact.yourwebdev@gmail.com>
2022-12-31 17:23:51 +0530

    front-end word complete

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,33 @@
 "use strict";
-console.log('Hello World');
+// Card Btn Open Animation 
+const noteAdderBtns = document.getElementById('noteAdderBtns');
+const noteBoxAniBtn = document.getElementById('noteBoxAniBtn');
+noteBoxAniBtn.addEventListener('click', () => {
+    noteAdderBtns.classList.toggle('active');
+});
+// All Card 
+const purpleDivOpener = document.getElementById('purpleDivOpener');
+const greenDivOpener = document.getElementById('greenDivOpener');
+const whiteDivOpener = document.getElementById('whiteDivOpener');
+const yelloDivOpener = document.getElementById('yelloDivOpener');
+// notes list
+const noteList = document.querySelector('.noteList');
+let num = 1;
+// all card types 
+const card = [purpleDivOpener, greenDivOpener, whiteDivOpener, yelloDivOpener];
+card.forEach((e) => {
+    e.addEventListener('click', () => {
+        // getting card type
+        const cardName = e.attributes[1].value;
+        // getting curent date
+        const d = new Date;
+        let dateNow = d.toLocaleDateString();
+        // blank card
+        let blankCard = `<div id="${num}" class="card ${cardName}"><div class="content"><div class="textContent"><p contenteditable>  Write Here </p></div><div class="editContent"><h4 class="date"> ${dateNow}</h4><h3 id="${num}-editBtn" class="editBtn"> <i class="fa-regular fa-circle-check"></i></h3>
+        </div></div><div class="editBox"><h1 class="moveBack"><i class="fa-solid fa-circle-arrow-left"></i></h1><div class="icons"><i class="fa-regular fa-pen-to-square"></i><i class="fa-regular fa-trash-can"></i></div></div></div>`;
+        num++;
+        let c = blankCard;
+        ;
+        noteList.innerHTML = c + noteList.innerHTML;
+    });
+});
--- src/typescript/script.ts
+++ src/typescript/script.ts
@@ -1,1 +1,39 @@
-console.log('Hello World')
+// Card Btn Open Animation 
+const noteAdderBtns: HTMLElement = document.getElementById('noteAdderBtns')!;
+const noteBoxAniBtn: HTMLElement = document.getElementById('noteBoxAniBtn')!;
+noteBoxAniBtn.addEventListener('click', () => {
+    noteAdderBtns.classList.toggle('active');
+})
+
+// All Card 
+const purpleDivOpener: HTMLElement = document.getElementById('purpleDivOpener')!;
+const greenDivOpener: HTMLElement = document.getElementById('greenDivOpener')!;
+const whiteDivOpener: HTMLElement = document.getElementById('whiteDivOpener')!;
+const yelloDivOpener: HTMLElement = document.getElementById('yelloDivOpener')!;
+
+// notes list
+const noteList: HTMLElement = document.querySelector('.noteList')!;
+let num: number = 1;
+
+// all card types 
+const card: Element[] = [purpleDivOpener, greenDivOpener, whiteDivOpener, yelloDivOpener];
+
+card.forEach((e) => {
+    e.addEventListener('click', () => {
+        // getting card type
+        const cardName: string = e.attributes[1].value;
+
+        // getting curent date
+        const d = new Date;
+        let dateNow = d.toLocaleDateString();
+
+        // blank card
+        let blankCard: string = `<div id="${num}" class="card ${cardName}"><div class="content"><div class="textContent"><p contenteditable>  Write Here </p></div><div class="editContent"><h4 class="date"> ${dateNow}</h4><h3 id="${num}-editBtn" class="editBtn"> <i class="fa-regular fa-circle-check"></i></h3>
+        </div></div><div class="editBox"><h1 class="moveBack"><i class="fa-solid fa-circle-arrow-left"></i></h1><div class="icons"><i class="fa-regular fa-pen-to-square"></i><i class="fa-regular fa-trash-can"></i></div></div></div>`;
+        num++;
+
+        let c = blankCard;;
+        noteList.innerHTML = c + noteList.innerHTML;
+
+    })
+});
