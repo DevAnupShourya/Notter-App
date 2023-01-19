42f744232a7a5b4c4a0f7dce6c7280cce3d113e7
Anup Shourya <contact.yourwebdev@gmail.com>
2023-01-19 16:59:39 +0530

    almost done

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,7 +9,6 @@ const noteList = document.querySelector('.noteList');
 // ? all card types 
 const cardTypes = [purpleDivOpener, greenDivOpener, whiteDivOpener, yelloDivOpener];
 // ? Gettting card type and rendering it in dom
-let cardType;
 cardTypes.forEach((e) => {
     e.addEventListener('click', () => {
         displayEditDiv(e);
@@ -38,37 +37,20 @@ let notes = JSON.parse(localStorage.getItem('notes') || '[]'); // All Text/Notes
 let notesTime = JSON.parse(localStorage.getItem('notesTime') || '[]'); // Last Editing Time
 let notesCardType = JSON.parse(localStorage.getItem('notesCardType') || '[]'); // Notes Type
 const addNote = (elem) => {
-    var _a, _b, _c;
+    var _a, _b, _c, _d, _e;
     const timeNow = new Date();
     // ? getting all data which to keep save
-    let note = elem.parentNode.parentNode.querySelector('p').innerText; // Text entered by the user
+    let note = (_b = (_a = elem === null || elem === void 0 ? void 0 : elem.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode) === null || _b === void 0 ? void 0 : _b.querySelector('p'); // Text entered by the user
     let noteTime = timeNow.toLocaleTimeString(); // current time
-    let cardType = (_c = (_b = (_a = elem.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement) === null || _c === void 0 ? void 0 : _c.classList[1]; // card type
+    let cardType = (_e = (_d = (_c = elem.parentElement) === null || _c === void 0 ? void 0 : _c.parentElement) === null || _d === void 0 ? void 0 : _d.parentElement) === null || _e === void 0 ? void 0 : _e.classList[1]; // card type
     // ? pushing all values in seprate arrays 
-    notes.push(note);
+    notes.push(note.innerText);
     notesTime.push(noteTime);
     notesCardType.push(cardType);
     // ? updating the localstorage
     updateLocalStorage();
     // ? displaying the new added notes in DOM
     displayNotes();
-};
-const replaceNote = (e) => {
-    var _a, _b, _c;
-    const timeNow = new Date();
-    // ? getting all data which to keep save
-    let note = e.parentNode.parentNode.querySelector('p').innerText; // Text entered by the user
-    let noteTime = timeNow.toLocaleTimeString(); // current time
-    let cardType = (_c = (_b = (_a = e.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement) === null || _c === void 0 ? void 0 : _c.classList[1]; // card type
-    const noteToReplace = note;
-    // ! first save the older note and find it in notes array then 
-    // const ith_to_replace: number = 1;
-    // replace notes.replace()
-    console.log(ith_to_replace);
-    // ? updating the localstorage
-    // updateLocalStorage();
-    // // ? displaying the new added notes in DOM
-    // displayNotes();
 };
 const updateLocalStorage = () => {
     // ? saving the new values after converting then into string :
@@ -106,14 +88,17 @@ const displayNotes = () => {
         noteList.innerHTML = currCard + noteList.innerHTML;
     });
 };
+// * index number to replace
+let card_i;
 const editDivOpen = (t) => {
-    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k;
+    var _a, _b, _c, _d, _e, _f;
     // ? hiding the editBox
-    (_b = (_a = t.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.style.display = 'none';
+    const ebox = (_a = t.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement;
+    ebox.style.display = 'none';
     // ? getting info about card
-    let card_html = (_d = (_c = t.parentElement) === null || _c === void 0 ? void 0 : _c.parentElement) === null || _d === void 0 ? void 0 : _d.parentElement;
-    let card_type = (_g = (_f = (_e = t.parentElement) === null || _e === void 0 ? void 0 : _e.parentElement) === null || _f === void 0 ? void 0 : _f.parentElement) === null || _g === void 0 ? void 0 : _g.classList[1];
-    let card_text = (_k = (_j = (_h = t.parentElement) === null || _h === void 0 ? void 0 : _h.parentElement) === null || _j === void 0 ? void 0 : _j.parentElement) === null || _k === void 0 ? void 0 : _k.children[0].children[0].children[0].textContent;
+    let card_html = (_c = (_b = t.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement) === null || _c === void 0 ? void 0 : _c.parentElement;
+    let card_text = (_f = (_e = (_d = t.parentElement) === null || _d === void 0 ? void 0 : _d.parentElement) === null || _e === void 0 ? void 0 : _e.parentElement) === null || _f === void 0 ? void 0 : _f.children[0].children[0].children[0].textContent;
+    card_i = get_i_by_text(card_text, notes);
     card_html.innerHTML = `
     <div class="content">
         <div class="textContent">
@@ -126,13 +111,31 @@ const editDivOpen = (t) => {
         </div>
     </div>`;
 };
+const replaceNote = (e) => {
+    var _a, _b, _c;
+    const timeNow = new Date();
+    // ? getting all data which to keep save
+    let note = e.parentNode.parentNode.querySelector('p').innerText; // Text entered by the user
+    let noteTime = timeNow.toLocaleTimeString(); // current time
+    let cardType = (_c = (_b = (_a = e.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement) === null || _c === void 0 ? void 0 : _c.classList[1]; // card type
+    // ? saving the index number which has to replace
+    const ith_to_replace = card_i;
+    // ? replacing the values
+    notes.splice(ith_to_replace, 1, note);
+    notesTime.splice(ith_to_replace, 1, noteTime);
+    notesCardType.splice(ith_to_replace, 1, cardType);
+    // ? updating the localstorage
+    updateLocalStorage();
+    // ? displaying the new added notes in DOM
+    displayNotes();
+};
 // ! delete the note
 const deleteNote = (t) => {
     var _a, _b, _c, _d, _e;
     // ? find the note to delete
     const noteToDelete = (_c = (_b = (_a = t.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement) === null || _c === void 0 ? void 0 : _c.children[0].children[0].children[0].textContent;
     // ? finding the index number which to delete
-    const ith_to_delete = know_ith_element(notes, noteToDelete);
+    const ith_to_delete = get_i_by_text(noteToDelete, notes);
     // ? delete that element from the given index number
     notes.splice(ith_to_delete, 1);
     notesTime.splice(ith_to_delete, 1);
@@ -142,59 +145,13 @@ const deleteNote = (t) => {
     const card = (_e = (_d = t.parentElement) === null || _d === void 0 ? void 0 : _d.parentElement) === null || _e === void 0 ? void 0 : _e.parentElement;
     card.style.display = 'none';
 };
-const know_ith_element = (arr, text_to_find) => {
-    let ith;
-    for (let j = 0; j < notes.length; j++) {
-        if (notes[j] == text_to_find) {
-            ith = j;
-        }
-    }
+const get_i_by_text = (t, arr) => {
+    const ith = arr.findIndex((e) => {
+        return e == t;
+    });
     return ith;
 };
 displayNotes();
-// for removing text when clicking on any input
-// const u_i = document.querySelectorAll('.user_input')!;
-// console.log(u_i);
-// u_i.forEach( (e) => {
-//     e.addEventListener('focus', (ev) => {
-//         // ev.target.innerText = '';
-//     })
-// }) 
-/*
-JSON.parse() -> s - o  A common use of JSON is to exchange data to/from a web server.When receiving data from a web server, the data is always a string. Parse the data with JSON.parse(), and the data becomes a JavaScript object.
-
-JSON.stringify() ->  common use of JSON is to exchange data to/from a web server.When sending data to a web server, the data has to be a string.Convert a JavaScript object into a string with JSON.stringify().
-*/
-/*
-<div class="card ${cardName}">
-    <div class="content">
-        <div class="textContent">
-            <p contenteditable>  Write Here </p>
-        </div>
-        <div class="editContent">
-            <h4 class="date"> ${dateNow}</h4>
-            <h3 id="${num}-editBtn" class="saveBtn" onclick='saveNote(this)'>
-                <i class="fa-regular fa-circle-check"></i>
-            </h3>
-        </div>
-    </div>
-</div>
-*/
-/*
-* It is for getting size of localstorage
-
-var _lsTotal = 0,
-    _xLen, _x;
-for (_x in localStorage) {
-    if (!localStorage.hasOwnProperty(_x)) {
-        continue;
-    }
-    _xLen = ((localStorage[_x].length + _x.length) * 2);
-    _lsTotal += _xLen;
-    console.log(_x.substr(0, 50) + " = " + (_xLen / 1024).toFixed(2) + " KB")
-};
-console.log("Total = " + (_lsTotal / 1024).toFixed(2) + " KB");
-*/
 // ? Card Btn Open Animation 
 const noteAdderBtns = document.getElementById('noteAdderBtns');
 const noteBoxAniBtn = document.getElementById('noteBoxAniBtn');
@@ -212,3 +169,36 @@ const hideEditBox = (t) => {
     const eb = t === null || t === void 0 ? void 0 : t.parentElement;
     eb.style.display = 'none';
 };
+// ? searching for notes
+const search = (t) => {
+    var _a, _b;
+    let searchQuery = t.value;
+    let notes_text = document.getElementsByTagName('p');
+    for (const n of notes_text) {
+        let note_text = n.innerText.toLowerCase();
+        let curr_Card = (_b = (_a = n.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement;
+        console.log(curr_Card);
+        if (note_text.includes(searchQuery)) {
+            curr_Card.style.display = 'flex';
+        }
+        else {
+            curr_Card.style.display = 'none';
+        }
+    }
+};
+// ? Showing used size in DOM
+const sizeShower = document.getElementById('size');
+// Copied from stack overflow
+let _lsTotal = 0, _xLen, _x;
+for (_x in localStorage) {
+    if (!localStorage.hasOwnProperty(_x)) {
+        continue;
+    }
+    _xLen = ((localStorage[_x].length + _x.length) * 2);
+    _lsTotal += _xLen;
+    // for each item of localstorage
+    // console.log(_x.substr(0, 50) + " = " + (_xLen / 1024).toFixed(2) + " KB")
+}
+;
+let used = `${(_lsTotal / 1024).toFixed(2)}KB`;
+sizeShower.innerText = used;
--- src/typescript/script.ts
+++ src/typescript/script.ts
@@ -3,17 +3,20 @@ const purpleDivOpener: HTMLElement = document.getElementById('purpleDivOpener')!
 const greenDivOpener: HTMLElement = document.getElementById('greenDivOpener')!;
 const whiteDivOpener: HTMLElement = document.getElementById('whiteDivOpener')!;
 const yelloDivOpener: HTMLElement = document.getElementById('yelloDivOpener')!;
+
 // ? notes lists showing div
 const noteList: HTMLElement = document.querySelector('.noteList')!;
+
 // ? all card types 
 const cardTypes: Element[] = [purpleDivOpener, greenDivOpener, whiteDivOpener, yelloDivOpener];
+
 // ? Gettting card type and rendering it in dom
-let cardType: string;
 cardTypes.forEach((e) => {
     e.addEventListener('click', () => {
         displayEditDiv(e);
     })
 })
+
 // ? displaying editor div of cardType
 const displayEditDiv = (e: Element) => {
     let card_Type: string = e.attributes[1].value;
@@ -38,38 +41,20 @@ let notes: string[] = JSON.parse(localStorage.getItem('notes') || '[]'); // All 
 let notesTime: string[] = JSON.parse(localStorage.getItem('notesTime') || '[]'); // Last Editing Time
 let notesCardType: string[] = JSON.parse(localStorage.getItem('notesCardType') || '[]'); // Notes Type
 
-const addNote = (elem: string) => {
+const addNote = (elem: HTMLElement) => {
     const timeNow: Date = new Date();
     // ? getting all data which to keep save
-    let note: string = elem.parentNode.parentNode.querySelector('p').innerText; // Text entered by the user
+    let note: HTMLParagraphElement = elem?.parentNode?.parentNode?.querySelector('p')!; // Text entered by the user
     let noteTime: string = timeNow.toLocaleTimeString(); // current time
-    let cardType: string = elem.parentElement?.parentElement?.parentElement?.classList[1]; // card type
+    let cardType: string = elem.parentElement?.parentElement?.parentElement?.classList[1]!; // card type
     // ? pushing all values in seprate arrays 
-    notes.push(note);
+    notes.push(note.innerText);
     notesTime.push(noteTime);
     notesCardType.push(cardType);
     // ? updating the localstorage
     updateLocalStorage();
     // ? displaying the new added notes in DOM
     displayNotes();
-}
-
-const replaceNote = (e : any) => {
-    const timeNow: Date = new Date();
-    // ? getting all data which to keep save
-    let note: string = e.parentNode.parentNode.querySelector('p').innerText; // Text entered by the user
-    let noteTime: string = timeNow.toLocaleTimeString(); // current time
-    let cardType: string = e.parentElement?.parentElement?.parentElement?.classList[1]; // card type
-    const noteToReplace : string = note;
-    // ! first save the older note and find it in notes array then 
-    // const ith_to_replace: number = 1;
-    // replace notes.replace()
-    console.log(ith_to_replace);
-    
-    // ? updating the localstorage
-    // updateLocalStorage();
-    // // ? displaying the new added notes in DOM
-    // displayNotes();
 }
 
 const updateLocalStorage = () => {
@@ -109,13 +94,16 @@ const displayNotes = () => {
         noteList.innerHTML = currCard + noteList.innerHTML;
     })
 }
+// * index number to replace
+let card_i : number;
 const editDivOpen = (t: HTMLElement) => {
     // ? hiding the editBox
-    t.parentElement?.parentElement?.style.display = 'none';
+    const ebox : HTMLElement = t.parentElement?.parentElement!;
+    ebox.style.display = 'none';
     // ? getting info about card
     let card_html: HTMLElement = t.parentElement?.parentElement?.parentElement!;
-    let card_type: string = t.parentElement?.parentElement?.parentElement?.classList[1]!;
     let card_text: string = t.parentElement?.parentElement?.parentElement?.children[0].children[0].children[0].textContent!;
+    card_i  = get_i_by_text(card_text , notes);
 
     card_html.innerHTML = `
     <div class="content">
@@ -130,12 +118,31 @@ const editDivOpen = (t: HTMLElement) => {
     </div>`;
 }
 
+const replaceNote = (e: any) => {
+    const timeNow: Date = new Date();
+    // ? getting all data which to keep save
+    let note: string = e.parentNode.parentNode.querySelector('p').innerText; // Text entered by the user
+    let noteTime: string = timeNow.toLocaleTimeString(); // current time
+    let cardType: string = e.parentElement?.parentElement?.parentElement?.classList[1]; // card type
+    
+    // ? saving the index number which has to replace
+    const ith_to_replace: number = card_i;
+    // ? replacing the values
+    notes.splice(ith_to_replace, 1, note);
+    notesTime.splice(ith_to_replace, 1, noteTime);
+    notesCardType.splice(ith_to_replace, 1, cardType);
+    // ? updating the localstorage
+    updateLocalStorage();
+    // ? displaying the new added notes in DOM
+    displayNotes();
+}
+
 // ! delete the note
 const deleteNote = (t: HTMLElement) => {
     // ? find the note to delete
     const noteToDelete: string = t.parentElement?.parentElement?.parentElement?.children[0].children[0].children[0].textContent!;
     // ? finding the index number which to delete
-    const ith_to_delete: number = know_ith_element(notes, noteToDelete);
+    const ith_to_delete: number = get_i_by_text(noteToDelete , notes);
     // ? delete that element from the given index number
     notes.splice(ith_to_delete, 1);
     notesTime.splice(ith_to_delete, 1);
@@ -145,64 +152,14 @@ const deleteNote = (t: HTMLElement) => {
     const card: HTMLElement = t.parentElement?.parentElement?.parentElement!;
     card.style.display = 'none';
 }
-const know_ith_element = (arr: string[], text_to_find: string) => {
-    let ith: number;
-    for (let j = 0; j < notes.length; j++) {
-        if (notes[j] == text_to_find) {
-            ith = j;
-        }
-    }
+
+const get_i_by_text = (t: string, arr: string[]) => {
+    const ith = arr.findIndex((e) => {
+        return e == t;
+    })
     return ith;
 }
-
 displayNotes();
-
-// for removing text when clicking on any input
-// const u_i = document.querySelectorAll('.user_input')!;
-// console.log(u_i);
-
-// u_i.forEach( (e) => {
-//     e.addEventListener('focus', (ev) => {
-//         // ev.target.innerText = '';
-//     })
-// }) 
-
-/*
-JSON.parse() -> s - o  A common use of JSON is to exchange data to/from a web server.When receiving data from a web server, the data is always a string. Parse the data with JSON.parse(), and the data becomes a JavaScript object.
-
-JSON.stringify() ->  common use of JSON is to exchange data to/from a web server.When sending data to a web server, the data has to be a string.Convert a JavaScript object into a string with JSON.stringify().
-*/
-/*
-<div class="card ${cardName}">
-    <div class="content">
-        <div class="textContent">
-            <p contenteditable>  Write Here </p>
-        </div>
-        <div class="editContent">
-            <h4 class="date"> ${dateNow}</h4>
-            <h3 id="${num}-editBtn" class="saveBtn" onclick='saveNote(this)'>
-                <i class="fa-regular fa-circle-check"></i>
-            </h3>
-        </div>
-    </div>
-</div>
-*/
-
-/*
-* It is for getting size of localstorage
-
-var _lsTotal = 0,
-    _xLen, _x;
-for (_x in localStorage) {
-    if (!localStorage.hasOwnProperty(_x)) {
-        continue;
-    }
-    _xLen = ((localStorage[_x].length + _x.length) * 2);
-    _lsTotal += _xLen;
-    console.log(_x.substr(0, 50) + " = " + (_xLen / 1024).toFixed(2) + " KB")
-};
-console.log("Total = " + (_lsTotal / 1024).toFixed(2) + " KB");
-*/
 
 // ? Card Btn Open Animation 
 const noteAdderBtns: HTMLElement = document.getElementById('noteAdderBtns')!;
@@ -213,7 +170,7 @@ noteBoxAniBtn.addEventListener('click', () => {
 
 // ? showing the settings div aniamation
 const showEditBox = (t: Element) => {
-    const eb: Element = t.parentElement?.parentElement?.parentElement?.children[1]!;
+    const eb = t.parentElement?.parentElement?.parentElement?.children[1]  as HTMLElement;
     eb.style.display = 'grid';
 }
 // ? hiding the settings div aniamation
@@ -221,3 +178,42 @@ const hideEditBox = (t: Element) => {
     const eb: HTMLElement = t?.parentElement!;
     eb.style.display = 'none';
 }
+
+// ? searching for notes
+const search = (t : HTMLInputElement) => {
+    let searchQuery : string = t.value;
+    let notes_text : HTMLCollectionOf<HTMLParagraphElement> = document.getElementsByTagName('p');
+    
+    for(const n of notes_text){
+        let note_text = n.innerText.toLowerCase();
+        let curr_Card : HTMLElement = n.parentElement?.parentElement?.parentElement!;
+        console.log(curr_Card);
+        
+        if(note_text.includes(searchQuery)){
+            curr_Card.style.display = 'flex';
+        }
+        else{
+            curr_Card.style.display = 'none';
+        }
+    }
+}
+
+
+// ? Showing used size in DOM
+
+const sizeShower : HTMLElement = document.getElementById('size')!;
+
+// Copied from stack overflow
+let _lsTotal = 0, _xLen, _x;
+for (_x in localStorage) {
+    if (!localStorage.hasOwnProperty(_x)) {
+        continue;
+    }
+    _xLen = ((localStorage[_x].length + _x.length) * 2);
+    _lsTotal += _xLen;
+    // for each item of localstorage
+    // console.log(_x.substr(0, 50) + " = " + (_xLen / 1024).toFixed(2) + " KB")
+};
+let used : string = `${(_lsTotal / 1024).toFixed(2)}KB`;
+sizeShower.innerText = used;
+
